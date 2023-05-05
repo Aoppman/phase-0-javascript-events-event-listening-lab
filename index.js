7914f7a66fe62103f60d36de62c55d4a5c66d60e
Andrew <a.oppman@gmail.com>
2023-05-05 13:31:20 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+//function addingEventListener() {}
+
+const input = document.getElementById('button')
+
+clickMessage = function() { 
+    alert('I was clicked') 
+};
+
+
 function addingEventListener() {
-}
+    input.addEventListener('click', clickMessage);
+};
